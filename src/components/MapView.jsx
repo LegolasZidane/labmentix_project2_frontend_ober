46b0e8b75d85f78b2f631ed8d59ff2af471d0f42
LegolasZidane <laraibaa6@gmail.com>
2025-09-07 01:08:22 +0530

    Route added

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,6 +1,7 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, Polyline } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import { useState, useEffect } from "react";
 
 const geoapifyKey = import.meta.env.VITE_GEOAPIFY_API_KEY;
 
@@ -13,28 +14,58 @@ L.Icon.Default.mergeOptions({
 
 export default function MapView({ originCoords, destinationCoords }){
 
+    const [ route, setRoute ] = useState(null);
+
+    useEffect(() => {
+
+        const fetchRoute = async () => {
+
+            if( !originCoords || !destinationCoords ) return;
+
+            const url = `https://api.geoapify.com/v1/routing?waypoints=${originCoords[0]},${originCoords[1]}|${destinationCoords[0]},${destinationCoords[1]}&mode=drive&apiKey=${geoapifyKey}`;
+
+            const res = await fetch(url);
+            const data = await res.json();
+
+            if( data.features && data.features.length > 0 ){
+                
+                const coords = data.features[0].geometry.coordinates[0].map((c) => [
+                    c[1],//lat
+                    c[0]
+                ]);
+                setRoute(coords);
+            }
+        };
+
+        fetchRoute();
+    }, [originCoords, destinationCoords]);
+
     return (
-            <MapContainer
-                center={[28.6139, 77.2090]} // Default: New Delhi
-                zoom={12}
-                style={{ height: "100%", width: "100%"}}
-            >
-                <TileLayer
-                    url={`https://maps.geoapify.com/v1/tile/dark-matter/{z}/{x}/{y}.png?apiKey=${geoapifyKey}`}
-                    attribution='&copy; <a href="https://www.openstreetmap.org/">OSM</a> contributors'
-                />
+        <MapContainer
+            center={originCoords || [28.6139, 77.2090]} // Default: New Delhi
+            zoom={12}
+            style={{ height: "100%", width: "100%"}}
+        >
+            <TileLayer
+                url={`https://maps.geoapify.com/v1/tile/dark-matter/{z}/{x}/{y}.png?apiKey=${geoapifyKey}`}
+                attribution='&copy; <a href="https://www.openstreetmap.org/">OSM</a> contributors'
+            />
 
-                {originCoords && (
-                    <Marker position={originCoords}>
-                        <Popup>Pickup</Popup>
-                    </Marker>
-                )}
+            {originCoords && (
+                <Marker position={originCoords}>
+                    <Popup>Pickup</Popup>
+                </Marker>
+            )}
 
-                {destinationCoords && (
-                    <Marker position={destinationCoords}>
-                        <Popup>Dropoff</Popup>
-                    </Marker>
-                )}
-                </MapContainer>
+            {destinationCoords && (
+                <Marker position={destinationCoords}>
+                    <Popup>Dropoff</Popup>
+                </Marker>
+            )}
+
+            {route && (
+                <Polyline positions={route} pathOptions={{color: "white", weight: 5}} />
+            )}
+        </MapContainer>
     );
 }
